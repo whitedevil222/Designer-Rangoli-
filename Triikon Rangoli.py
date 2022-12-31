05ac3304462eedd75234d00663d17566abfba0b5
Whitedevil <105511358+whitedevil222@users.noreply.github.com>
2022-12-31 18:46:47 +0530

    Add files via upload

---
+++ Triikon Rangoli.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.bgcolor ("black")
+
+t=turtle.Turtle()
+t.speed(200)
+t.pensize(2)
+
+for i in range(500):
+    t.pencolor("teal")
+    t.fd(i)
+    t.rt(144)
+    t.fd(i)
+    t.rt(90)
+    
